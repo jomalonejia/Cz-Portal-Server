7762038f1765944a1ce930ae988084d04fb4a673
jomalonejia <jo_malone@outlook.com>
2017-09-13 16:29:05 +0800

    test something

---
+++ rabbit-hello/src/main/java/com/cz/config/RabbitHelloApplication.java
@@ -0,0 +1,12 @@
+package com.cz.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitHelloApplication.class, args);
+	}
+}
---
+++ rabbit-hello/src/test/java/com/cz/config/RabbitHelloApplicationTests.java
@@ -0,0 +1,21 @@
+package com.cz.config;
+
+import com.cz.config.demo.Sender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RabbitHelloApplication.class)
+public class RabbitHelloApplicationTests {
+
+	@Autowired
+	private Sender sender;
+
+	@Test
+	public void hello() throws Exception {
+		sender.send();
+	}
+}
